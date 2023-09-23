2efa3b7959cdd8fa50059714e2dbf1f4ab8e8741
Afrie Irham <afrieirham.work@gmail.com>
2023-09-23 20:27:12 +0800

    chore: implement social icons with links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,8 @@
+import { TailwindColors } from "@/types";
 import { Source_Serif_4 } from "next/font/google";
 import React from "react";
 
 const sourceSerif4 = Source_Serif_4({ subsets: ["latin"] });
-
-type TailwindColors =
-  | "slate"
-  | "gray"
-  | "zinc"
-  | "neutral"
-  | "stone"
-  | "red"
-  | "orange"
-  | "amber"
-  | "yellow"
-  | "lime"
-  | "green"
-  | "emerald"
-  | "teal"
-  | "cyan"
-  | "sky"
-  | "blue"
-  | "indigo"
-  | "violet"
-  | "purple"
-  | "fuchsia"
-  | "pink"
-  | "rose";
 
 export default function Home() {
   const services = [
@@ -38,23 +15,27 @@ export default function Home() {
       "https://firebasestorage.googleapis.com/v0/b/pakejkahwincom.appspot.com/o/services%2F314825016_644859110439104_9041130988362878036_n.jpg?alt=media&token=8add9dc9-82ef-422c-849f-64e911a620bb",
     socials: [
       {
-        id: "instagram",
+        name: "instagram",
         link: "https://www.instagram.com/urbanparkhall.my/",
       },
       {
-        id: "whatsapp",
+        name: "whatsapp",
         link: "https://wa.me/60121231234",
       },
       {
-        id: "maps",
+        name: "maps",
         link: "https://wa.me/60121231234",
       },
       {
-        id: "tiktok",
+        name: "tiktok",
         link: "https://wa.me/60121231234",
       },
       {
-        id: "website",
+        name: "website",
+        link: "https://wa.me/60121231234",
+      },
+      {
+        name: "facebook",
         link: "https://wa.me/60121231234",
       },
     ],
@@ -99,13 +80,18 @@ export default function Home() {
                         {service.type.name}
                       </Badge>
                     </td>
-                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium ">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit<span className="sr-only">, {service.name}</span>
-                      </a>
+                    <td className="relative flex items-center justify-end space-x-1 whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium">
+                      {service.socials
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                        .map((social) => (
+                          <a
+                            href={social.link}
+                            className="mt-2"
+                            target="_blank"
+                          >
+                            <SocialIcon name={social.name} />
+                          </a>
+                        ))}
                     </td>
                   </tr>
                 ))}
@@ -158,3 +144,115 @@ function Badge({
     </span>
   );
 }
+
+const SocialIcon = ({ name }: { name: string }) => {
+  const size = "h-6 w-6";
+
+  if (name === "facebook") {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-brand-facebook fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+      </svg>
+    );
+  }
+
+  if (name === "instagram") {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-brand-instagram fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+        <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+        <path d="M16.5 7.5l0 .01" />
+      </svg>
+    );
+  }
+
+  if (name === "maps")
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-map-pin fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+      </svg>
+    );
+
+  if (name === "tiktok")
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-brand-tiktok fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" />
+      </svg>
+    );
+
+  if (name === "website")
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-world fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+        <path d="M3.6 9h16.8" />
+        <path d="M3.6 15h16.8" />
+        <path d="M11.5 3a17 17 0 0 0 0 18" />
+        <path d="M12.5 3a17 17 0 0 1 0 18" />
+      </svg>
+    );
+
+  if (name === "whatsapp")
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`icon icon-tabler icon-tabler-brand-whatsapp fill-none stroke-stone-500 ${size}`}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+        <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+      </svg>
+    );
+
+  return null;
+};
