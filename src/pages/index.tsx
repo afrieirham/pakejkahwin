4d9a72e0755acb51a94748bf0beb768e523c0ac9
Afrie Irham <afrieirham.work@gmail.com>
2023-09-23 16:02:46 +0800

    view: customize design system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Source_Serif_4 } from "next/font/google";
+
+const sourceSerif4 = Source_Serif_4({ subsets: ["latin"] });
+
 export default function Home() {
   return (
     <div>
-      <p>hello world</p>
+      <p className="bg-base-100">hello world</p>
+      <p className="bg-base-200">hello world</p>
+      <p className="bg-base-300">hello world</p>
+      <button className="btn btn-primary">Hello</button>
+      <button className="btn btn-secondary">Hello</button>
+      <button className="btn btn-accent">Hello</button>
+      <h1 className={`${sourceSerif4.className} font-semibold`}>Hello World</h1>
+      <h2 className={`${sourceSerif4.className} font-semibold`}>Hello World</h2>
+      <h3 className={`${sourceSerif4.className} font-semibold`}>Hello World</h3>
+      <h4 className={`${sourceSerif4.className} font-semibold`}>Hello World</h4>
+      <h5 className={`${sourceSerif4.className} font-semibold`}>Hello World</h5>
+      <h6 className={`${sourceSerif4.className} font-semibold`}>Hello World</h6>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,30 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Source Serif 4", "serif"],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["retro"],
+    themes: [
+      {
+        retro: {
+          ...require("daisyui/src/theming/themes")["[data-theme=retro]"],
+          "base-100": "#f6f4ec",
+          "base-200": "#EDE9D9",
+          "base-300": "#E4DEC8",
+          primary: "#545748",
+          ".btn-primary": {
+            color: "#f6f4ec",
+          },
+        },
+      },
+    ],
   },
 };
 export default config;
